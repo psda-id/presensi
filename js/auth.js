8eba4e32858a3988b67a56056c3061f038d769e0
psda-id <psda.id2@gmail.com>
2026-07-31 23:29:53 +0700

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,51 @@
+/* =====================================================
+   auth.js
+===================================================== */
+
+const Auth = (()=>{
+
+    async function login(username,password){
+
+        const result=
+            await API.login(username,password);
+
+        if(result.status==="success"){
+
+            localStorage.setItem(
+                "token",
+                result.token
+            );
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    function logout(){
+
+        localStorage.removeItem("token");
+
+        location.href="index.html";
+
+    }
+
+    function isLogin(){
+
+        return localStorage.getItem("token")!=null;
+
+    }
+
+    return{
+
+        login,
+
+        logout,
+
+        isLogin
+
+    };
+
+})();
